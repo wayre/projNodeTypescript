1bf24715c6e2c3590f192334bba5a5e93d6df402
Wayre Avelar <wayre1@gmail.com>
2024-08-06 10:57:06 -0300

    feat: add lint

--- src/utils/module-alias.ts
+++ src/utils/module-alias.ts
@@ -3,7 +3,7 @@ import moduleAlias from 'module-alias';
 
 const files = path.resolve(__dirname, '../..');
 
-moduleAlias.addAlias({
-  '@src': path.join('files', 'src'),
-  '@test': path.join('files', 'test')
-})';'
+moduleAlias.addAliases({
+  '@src': path.join(files, 'src'),
+  '@test': path.join(files, 'test')
+});
